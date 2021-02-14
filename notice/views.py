903c211fa296ff372254464937178712d8865837
sachin kale <sskaryan612@gmail.com>
2021-02-14 16:36:40 +0530

    Wrongly done code, messed up with the original code

--- notice/views.py
+++ notice/views.py
@@ -5,11 +5,11 @@ from datetime import date
 from .forms import ImageForm
 import datetime
 
-def index(request):
-    image = Image.objects.all()
-    no_of_image = len(image)
-    params = {'product': image, 'number': no_of_image}
-    return render(request, 'index.html', params)
+# def index(request):
+#     image = Image.objects.all()
+#     no_of_image = len(image)
+#     params = {'product': image, 'number': no_of_image}
+#     return render(request, 'index.html', params)
 
 def administrator(request):
     image = Image.objects.all()
@@ -22,22 +22,17 @@ def administrator(request):
     params = {'form':form, 'image':image}
     return render(request, 'admin.html',params)
 
-def calendar(request):
-    return render(request, 'calendar.html')
-
 def about(request):
     return render(request, 'about.html')
-
 
 def contact(request):
     return render(request, 'contact.html')
 
-def test(request):
+def index(request):
     image = Image.objects.all()
     no_of_image = len(image)
     dates = date.today()
-    # times = date.time
     times = datetime.datetime.now().strftime('%H:%M:%S')
     params = {'product': image, 'number': no_of_image, 'dates':dates, 'times':times}
-    return render(request, 'test.html', params)
+    return render(request, 'index.html', params)
 
