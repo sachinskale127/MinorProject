7a395131d1d80853b4efdb4590e312f09536fac5
sachin kale <sskaryan612@gmail.com>
2021-02-14 16:49:26 +0530

    Revert "Wrongly done code, messed up with the original code"
    This reverts commit 903c211fa296ff372254464937178712d8865837.

--- notice/urls.py
+++ notice/urls.py
@@ -7,9 +7,9 @@ from django.contrib import admin
 urlpatterns = [
     path('', views.index, name='index'),
     path('myadmin/', views.administrator, name='administrator'),
-    # path('calendar/', views.calendar, name='calendar'),
+    path('calendar/', views.calendar, name='calendar'),
     path('AboutUs/', views.about, name='about'),
     path('ContactUs/', views.contact, name='contact'),
-    # path('test/', views.test, name='test'),
+    path('test/', views.test, name='test'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
